a92f93e2e773248d90c76091a93f24a04a1a524c
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-07-02 21:37:53 +0600

    Hero fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto mt-28">
+    <section className="relative w-full h-screen mx-auto mt-28 mb-5">
       <div className="relative w-full h-screen mx-auto">
         <div
           className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
@@ -21,6 +21,11 @@ const Hero = () => {
           </div>
         </div>
         <ComputersCanvas />
+      </div>
+      <div className="absolute xs:bottom-10 bottom-36 w-full flex justify-center items-center">
+        <div className="w-[78px] h-[48px] rounded-2xl border-4 border-secondary flex justify-center items-start px-16 py-2 cursor-pointer hover:bg-[#dd3232] hover:border-black">
+          ⬇️Resume
+        </div>
       </div>
     </section>
   );
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,13 +5,13 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./cyberpunk_desk/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-20, 60, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -20,9 +20,11 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={1.1}
+        // position={[0, -3.25, -1.5]}
+        // rotation={[-0.01, -0.2, -0.1]}
+        position={[9.4, -5, 3]}
+        rotation={[0.15, 0.35, -0.05]}
       />
     </mesh>
   );
